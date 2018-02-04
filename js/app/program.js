a8cbfb69f8c12f0e088ebc217cf9443d8a9f93b5
Wiktoria Mikołajów <mikolajow.w@gmail.com>
2018-02-04 13:24:28 +0100

    Finish project

---
+++ js/app/program.js
@@ -0,0 +1,29 @@
+var os = require('os');
+var OSinfo = require('../modules/OSinfo');
+var printTime = require('../modules/time');
+
+process.stdin.setEncoding('utf-8');
+
+console.log('Available commands: sayHello, /getOSinfo, /exit');
+
+process.stdin.on('readable', function() {
+    var input = process.stdin.read();
+    if (input !== null) {
+        var instruction = input.trim();
+
+        switch(instruction) {
+            case '/exit':
+                process.stdout.write('Quitting App! \n');
+                process.exit();
+                break;
+            case 'sayHello':
+                process.stdout.write('Hello!\n');
+                break;
+            case '/getOSinfo':
+                OSinfo.print();
+                break;
+            default:
+                process.stderr.write('Wrong instruction \n');
+        }
+    }
+});
